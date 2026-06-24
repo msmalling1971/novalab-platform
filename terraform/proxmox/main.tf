2b1fda8d6e3b7ee91a8a46053e2a3e1484016de9
msmalling1971 <msmalling@gmail.com>
2026-06-23 22:53:40 -0400

    Add static IP and SSH key support to Proxmox Ubuntu module

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -37,3 +37,25 @@ module "ubuntu_test_02" {
 
   environment = var.environment
 }
+module "k3s_test_01" {
+  source = "../modules/proxmox-ubuntu-vm"
+
+  vm_name       = "k3s-test-01"
+  vm_id         = 130
+  target_node   = "prox1"
+  template_name = "ubuntu-2404-template"
+
+  cpu_cores    = 2
+  memory_mb    = 4096
+  disk_size    = 40
+  storage_name = "local-lvm"
+  bridge       = "vmbr0"
+
+  ci_user     = "novaadmin"
+  ci_password = "ChangeMe123!"
+
+  ipconfig0 = "ip=192.168.50.130/24,gw=192.168.50.1"
+
+  ssh_public_key = file("/mnt/c/Users/msmalling/.ssh/id_ed25519.pub")
+  environment    = var.environment
+}
--- terraform/proxmox/outputs.tf
+++ terraform/proxmox/outputs.tf
@@ -9,3 +9,10 @@ output "netbox_test_vm_id" {
 output "environment" {
   value = var.environment
 }
+output "k3s_test_01_vm_name" {
+  value = module.k3s_test_01.vm_name
+}
+
+output "k3s_test_01_vm_id" {
+  value = module.k3s_test_01.vm_id
+}
